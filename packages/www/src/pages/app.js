8cac51e62ad7bf3289e334fc7bd84384c82b5360
ShaheerBin-Afaq <47066837+ShaheerBinAfaq@users.noreply.github.com>
2022-09-30 10:40:04 +0500

    added graphql

--- packages/www/src/pages/app.js
+++ packages/www/src/pages/app.js
@@ -2,31 +2,7 @@ import React, { useContext } from "react";
 import { Router, Link } from "@reach/router";
 import { IdentityContext } from '../../identity-context';
 import { Button, Container, Flex, Heading, NavLink } from "theme-ui";
-
-
-let Dash = () => {
-    const {user, identity: netlifyIdentity} = useContext(IdentityContext);
-    return (
-        <div>
-            <Flex as="nav">
-                <NavLink as={Link} to="/" p={2}>
-                    Home
-                </NavLink>
-                <NavLink as={Link} to={"/app"} p={2}>
-                    Dashboard
-                </NavLink>
-                {user && (
-                    <NavLink href="#!" onClick={() => {
-                        netlifyIdentity.logout();
-                    }} p={2}>
-                        Logout {user.user_metadata.full_name}
-                    </NavLink>
-                )}
-                </Flex>
-                Dash has user: {user && user.user_metadata.full_name}
-        </div>
-    );
-};
+import Dash from '../components/dashboard';
 
 let DashLoggedOut = props => {
     const { identity: netlifyIdentity } = useContext(IdentityContext);
